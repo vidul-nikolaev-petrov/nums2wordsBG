3d269e6e6c95b86fd9269ad4440ab0aa4ec40d3a
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2025-10-15 19:29:18 +0300

    Define more strict TS types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,22 +1,23 @@
 declare module "nums2words-bg" {
     /** Родове */
-    export type Gender = "m" | "f" | "n"; // мъжки, женски, среден
+    type Gender = "m" | "f" | "n"; // мъжки, женски, среден
+    type TimeString = `${number}:${number}:${number}`;
 
     /** Общи опции за граматика и разделители */
-    export interface GrammarOptions {
+    interface GrammarOptions {
         union?: string; // напр. "и"
         comma?: string; // допълнителна опция за запетаи
     }
 
     /** Основни опции за преобразуване на число в думи */
-    export interface Num2WordsOptions {
+    interface Num2WordsOptions {
         gender?: Gender;
         grammar?: GrammarOptions;
         separator?: string; // напр. ", "
     }
 
     /** Опции за валута */
-    export interface CurrencyOptions extends Num2WordsOptions {
+    interface CurrencyOptions extends Num2WordsOptions {
         currency?: "bgn" | "eur" | "usd" | "rub" | "btc" | "cny" | string;
         labelBig?: string; // напр. "лв.", "евро"
         labelSmall?: string; // напр. "ст.", "цента"
@@ -25,7 +26,7 @@ declare module "nums2words-bg" {
     }
 
     /** Опции за време */
-    export interface TimeOptions extends Num2WordsOptions {
+    interface TimeOptions extends Num2WordsOptions {
         displayHour?: boolean;
         displayMinute?: boolean;
         displaySecond?: boolean;
@@ -35,14 +36,14 @@ declare module "nums2words-bg" {
     }
 
     /** Опции за дата / период */
-    export interface DateOptions extends Num2WordsOptions {
+    interface DateOptions extends Num2WordsOptions {
         format?: string; // напр. "a/y/d/m" или "w,d"
         labels?: Partial<
             Record<"day" | "week" | "month" | "year" | "age", string>
         >;
     }
 
-    /** Функция за добавяне на нова валута */
+    /** Интерфейси за добавяне на нова валута */
     interface CustomCurrency {
         labelBig: string;
         labelSmall: string;
@@ -52,12 +53,12 @@ declare module "nums2words-bg" {
         gender: Record<1 | 2, Record<string, string>>;
     }
 
-    /** Основен интерфейс на библиотеката */
-    export interface CustomCurrencyMap {
+    interface CustomCurrencyMap {
         [key: string]: CustomCurrency;
     }
 
-    export interface Num2WordsBG {
+    /** Основен интерфейс на библиотеката */
+    interface Num2WordsBG {
         (value: string, options?: Num2WordsOptions): string;
 
         /** Преобразува число във валута (BGN, EUR, USD, BTC, др.) */
@@ -68,7 +69,7 @@ declare module "nums2words-bg" {
         ): string;
 
         /** Преобразува време (HH:MM:SS) */
-        time(value: string, options?: TimeOptions): string;
+        time(value: TimeString, options?: TimeOptions): string;
 
         /** Преобразува дата / период */
         date(value: string, options?: DateOptions): string;
